e34ca78dd3c1ddaf71b07508b3977f372bb55b89
nyagaderrick <nyagad@icloud.com>
2023-11-09 13:07:46 +0300

    updates

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
 web: uvicorn main:app --host 0.0.0.0 --port $PORT
 
-heroku-postbuild:
-  - apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran
+
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import numpy as np
-import pandas as pd
-import joblib
+import pickle
+import json
+
 
 app = FastAPI()
 
@@ -17,35 +17,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class ModelInput(BaseModel):
-    Pregnancies: int
-    Glucose: int
-    BloodPressure: int
-    SkinThickness: int
-    Insulin: int
-    BMI: float
-    Age: int
+class model_input(BaseModel):
+    
+    Pregnancies : int
+    Glucose : int
+    BloodPressure : int
+    SkinThickness : int
+    Insulin : int
+    BMI : float
+    Age : int
+    
 
-# Load the scaler for standardization
-scaler = joblib.load('scaler.joblib')
+# loading the saved model
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
 
-# Load the saved Scikit-Learn model
-diabetes_model = joblib.load('diabetes_model.joblib')
 
 @app.post('/diabetes_prediction')
-def diabetes_pred(input_parameters: ModelInput):
+def diabetes_pred(input_parameters : model_input):
     
-    # Convert input parameters to DataFrame
-    input_df = pd.DataFrame([input_parameters.dict()])
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
     
-    # Standardize the features
-    input_scaled = scaler.transform(input_df)
-    
-    # Make predictions using the loaded model
-    predictions = diabetes_model.predict(input_scaled)
-    
-    # Convert predictions to text labels
-    outcome_mapping = {0: 'This person is diabetic', 1: 'This person is not diabetic'}
-    text_labels = [outcome_mapping[label] for label in predictions.flatten()]
+    preg = input_dictionary['Pregnancies']
+    glu = input_dictionary['Glucose']
+    bp = input_dictionary['BloodPressure']
+    skin = input_dictionary['SkinThickness']
+    insulin = input_dictionary['Insulin']
+    bmi = input_dictionary['BMI']
+    age = input_dictionary['Age']
 
-    return {'prediction': text_labels[0]}
+
+    input_list = [preg, glu, bp, skin, insulin, bmi,age]
+    
+    prediction = diabetes_model.predict([input_list])
+    
+    if prediction[0] == 0:
+        return 'The person is not Diabetic'
+    
+    else:
+        return 'The person is Diabetic'
