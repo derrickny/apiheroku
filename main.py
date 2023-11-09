b4ed7adfaa2ec7636d198e1b31953a742317665c
Derrick Nyaga <nyagad@icloud.com>
2023-11-09 12:20:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import json
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model
-from sklearn.preprocessing import StandardScaler
-
-
+import joblib
 
 app = FastAPI()
 
@@ -31,12 +27,10 @@ class ModelInput(BaseModel):
     Age: int
 
 # Load the scaler for standardization
-scaler = StandardScaler()
-scaler.mean_ = np.array([3.845052083, 120.8945313, 69.10546875, 20.53645833, 79.79947917, 31.99257813, 33.24088542])
-scaler.scale_ = np.array([3.369578062, 31.95179591, 19.34320163, 15.9418294, 115.2440024, 7.88416031, 11.76023154])
+scaler = joblib.load('scaler.joblib')
 
-# Load the saved neural network model
-diabetes_model = load_model('diabetes_model.h5')
+# Load the saved Scikit-Learn model
+diabetes_model = joblib.load('diabetes_model.joblib')
 
 @app.post('/diabetes_prediction')
 def diabetes_pred(input_parameters: ModelInput):
@@ -48,12 +42,10 @@ def diabetes_pred(input_parameters: ModelInput):
     input_scaled = scaler.transform(input_df)
     
     # Make predictions using the loaded model
-    predictions = (diabetes_model.predict(input_scaled) > 0.5).astype(int)
+    predictions = diabetes_model.predict(input_scaled)
     
     # Convert predictions to text labels
     outcome_mapping = {0: 'This person is diabetic', 1: 'This person is not diabetic'}
     text_labels = [outcome_mapping[label] for label in predictions.flatten()]
 
     return {'prediction': text_labels[0]}
-
-#print(sys.version)
